21bc16891df21877020c9242f153b0527384c6aa
lut0209vn <lut0209vn@gmail.com>
2018-01-05 21:31:24 +0700

    a

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,11 @@
 'use strict';
-
+$(".navbar-toggle").click(function(){
+  $(".nav-mobile").stop().slideToggle();
+});
+if( $(window).width()> 992){
+  $(".nav-mobile").css("display","none");
+}
+// ratting
 /* HELPER: Checks Whether an Element Exists
 ----------------------------------------------------------------------------------------------------*/
 (function( $ ) {
@@ -16,3 +22,40 @@
 
 })( jQuery );
 
+$('.md-rating-3').raty({ score: 3 });
+$('.md-rating-4').raty({ score: 4 });
+$('.md-rating-5').raty({ score: 5 });
+
+// slider ranger
+ jQuery("#Slider1").slider({
+   from: 0,
+    to: 10000,
+   step: 1,
+   smooth: true, round: 0,
+   dimension: "",
+   skin: "plastic" ,
+});
+jQuery("#Slider2").slider({
+  from: 0,
+   to: 10000,
+  step: 1,
+  smooth: true, round: 0,
+  dimension: "",
+  skin: "plastic" ,
+});
+
+$("#btn-minus").click(function(){
+  var a=parseInt(document.getElementById("value-input").value)
+  if(a>1){
+    a--;
+    document.getElementById("value-input").value=a;
+  }
+});
+$("#btn-plus").click(function(){
+  var a=parseInt(document.getElementById("value-input").value)
+    a++;
+    document.getElementById("value-input").value=a;
+});
+
+//slider homepage
+$(".owl-slider-1").owlCarousel();
